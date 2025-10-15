f9152b34d6144b5df8b880230f63918fb86ac98b
nikapiskunova3-sys <nikapiskunova3@gmail.com>
2025-10-15 22:15:38 +0300

    Delete src/lab2 directory

--- src/lab2/Main.java
+++
@@ -1,9 +0,0 @@
-package lab2;
-
-public class Main {
-    public static void main(String[] args) {
-        Author author = new Author("Veronika", "veronika@mail.ru", 'Q');
-
-        System.out.println(author);
-    }
-}
