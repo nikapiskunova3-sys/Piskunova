ce350aee3df92fa70c1487cff7798c08095c2b2d
nikapiskunova3-sys <nikapiskunova3@gmail.com>
2025-11-28 20:31:46 +0300

    Add files via upload

---
+++ lab6_part_2/IStringProcessor.java
@@ -0,0 +1,7 @@
+package lab6_part_2;
+
+public interface IStringProcessor {
+    int countChars(String s);
+    String oddPositions(String s);
+    String invert(String s);
+}
---
+++ lab6_part_2/Printable.java
@@ -0,0 +1,5 @@
+package lab6_part_2;
+
+public interface Printable {
+    void print();
+}
